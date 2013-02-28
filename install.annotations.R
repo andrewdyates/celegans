ba8e42f33852ee5b2a88187e5ce32880d31b5768
Andrew D. Yates <andrewyates.name@gmail.com>
2013-02-28 14:48:55 -0500

    install annotations

--- install.annotations.R
+++ install.annotations.R
@@ -1,5 +1,15 @@
 download.file("http://brainarray.mbni.med.umich.edu/Brainarray/Database/CustomCDF/16.0.0/entrezg.download/celegansceentrezgprobe_16.0.0.tar.gz", "celegansceentrezgprobe_16.0.0.tar.gz")
 download.file("http://brainarray.mbni.med.umich.edu/Brainarray/Database/CustomCDF/16.0.0/entrezg.download/celegansceentrezg.db_16.0.0.tar.gz", "celegansceentrezg.db_16.0.0.tar.gz")
+
+source("http://bioconductor.org/biocLite.R")
+biocLite("org.Ce.eg.db")
+
+# biocLite("celegansprobe")
+# biocLite("celegans.db")
+# CDF files describe which probes are part of which probe set.
+# http://masker.nci.nih.gov/ev/
+
+
 install.packages("celegansceentrezgprobe_16.0.0.tar.gz", repos=NULL, type="source")
 install.packages("celegansceentrezg.db_16.0.0.tar.gz", repos=NULL, type="source")
 
