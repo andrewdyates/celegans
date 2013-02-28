edfc7e9b7464110b66b6c1623164956ef8827be1
Andrew D. Yates <andrewyates.name@gmail.com>
2013-02-28 14:36:20 -0500

    project init

---
+++ install.annotations.R
@@ -0,0 +1,31 @@
+download.file("http://brainarray.mbni.med.umich.edu/Brainarray/Database/CustomCDF/16.0.0/entrezg.download/celegansceentrezgprobe_16.0.0.tar.gz", "celegansceentrezgprobe_16.0.0.tar.gz")
+download.file("http://brainarray.mbni.med.umich.edu/Brainarray/Database/CustomCDF/16.0.0/entrezg.download/celegansceentrezg.db_16.0.0.tar.gz", "celegansceentrezg.db_16.0.0.tar.gz")
+install.packages("celegansceentrezgprobe_16.0.0.tar.gz", repos=NULL, type="source")
+install.packages("celegansceentrezg.db_16.0.0.tar.gz", repos=NULL, type="source")
+
+library(celegansceentrezgprobe)
+library(celegansceentrezg.db)
+
+ls("package:celegansceentrezgprobe")
+ls("package:celegansceentrezg.db")
+
+
+## > install.packages("celegansceentrezg.db_16.0.0.tar.gz", repos=NULL, type="source")
+## * installing *source* package ‘celegansceentrezg.db’ ...
+## ** R
+## ** inst
+## ** preparing package for lazy loading
+## ** help
+## *** installing help indices
+## ** building package indices
+## ** testing if installed package can be loaded
+## Error : .onLoad failed in loadNamespace() for 'celegansceentrezg.db', details:
+##   call: sqliteExecStatement(con, statement, bind.data)
+##   error: RS-DBI driver: (error in statement: no such table: chrlengths)
+## Error: loading failed
+## Execution halted
+## ERROR: loading failed
+## * removing ‘/nfs/01/osu6683/R/R-2.15.1/library/celegansceentrezg.db’
+## Warning message:
+## In install.packages("celegansceentrezg.db_16.0.0.tar.gz", repos = NULL,  :
+##   installation of package ‘celegansceentrezg.db_16.0.0.tar.gz’ had non-zero exit status
