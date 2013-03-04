f3b3260da6d5404144e4937d2ef1616051843da3
Andrew D. Yates <andrewyates.name@gmail.com>
2013-03-04 13:43:16 -0500

    fixed gse9*65 to gse9665

--- norm_gse9665.R
+++ norm_gse9665.R
@@ -1,8 +1,8 @@
 library(SCAN.UPC)
 library(celegansceentrezgprobe)
 
-fname.ptn <- "/fs/lustre/share/yates.c.elegans.sp13/GSE9965_RAW/*.cel.gz"
+fname.ptn <- "/fs/lustre/share/yates.c.elegans.sp13/GSE9665_RAW/*.cel.gz"
 out.fpath <- "/fs/lustre/share/yates.c.elegans.sp13/"
-GSE9665.ALL.SCAN = SCAN(fname.ptn, probeSummaryPackage=celegansceentrezgprobe, outFilePath=paste0(out.fpath, "GSE9965.ALL.SCAN.txt"))
-GSE9665.ALL.UPC = UPC(fname.ptn, probeSummaryPackage=celegansceentrezgprobe, outFilePath=paste0(out.fpath, "GSE9965.ALL.UPC.txt"))
-save(GSE9965.ALL.SCAN, GSE9965.ALL.UPC, file=paste0(out.fpath,"GSE9965.ALL.RData"))
+GSE9665.ALL.SCAN = SCAN(fname.ptn, probeSummaryPackage=celegansceentrezgprobe, outFilePath=paste0(out.fpath, "GSE9665.ALL.SCAN.txt"))
+GSE9665.ALL.UPC = UPC(fname.ptn, probeSummaryPackage=celegansceentrezgprobe, outFilePath=paste0(out.fpath, "GSE9665.ALL.UPC.txt"))
+save(GSE9665.ALL.SCAN, GSE9665.ALL.UPC, file=paste0(out.fpath,"GSE9665.ALL.RData"))
