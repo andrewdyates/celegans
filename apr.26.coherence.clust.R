5d156d4791d17a46ecf9cd6e5e22d9c154cb9cba
Andrew Yates <andrewyates.name@gmail.com>
2013-04-26 20:12:10 -0400

    cluster finding plots

---
+++ apr.26.coherence.clust.R
@@ -0,0 +1,204 @@
+library(Biobase)
+load("../trans.genes.apr17.gsplom.RData")
+load("../apr16.genelevel.depM.RData")
+load("../apr16.genelevel.exprs.RData")
+source("~/Dropbox/biostat/git_repos/dependency_glyph_splom/lib.R")
+
+make.cls.dist.M <- function() {
+  cls.enum.names <- c("hih","pc","lil","unl","hil","nc","lih","na")
+  r1 <- c(0,1,2,1,2,3,2,2)
+  r2 <- c(1,0,1,2,3,4,3,2)
+  r3 <- c(2,1,0,1,2,3,2,2)
+  r4 <- c(1,2,1,0,1,2,1,2)
+  r5 <- c(2,3,2,1,0,1,2,2)
+  r6 <- c(3,4,3,2,1,0,1,2)
+  r7 <- c(2,3,2,1,2,1,0,2)
+  r8 <- c(2,2,2,2,2,2,2,0)
+  G.DIST.TABLE <- rbind(r1,r2,r3,r4,r5,r6,r7,r8)
+  rownames(G.DIST.TABLE) <- cls.enum.names
+  colnames(G.DIST.TABLE) <- cls.enum.names
+  G.DIST.TABLE
+}
+G.DIST <- make.cls.dist.M()
+
+# coherences is max 1-(1/4) mean hamming dist to single class
+# BOOL_ENUM = {0:'NA', 1:'XiY', 2:'PC', 3:'YiX', 4:'UNL', 5:'MX', 6:'NC', 7:'OR'}
+# NOTE: NA:8 in R CLS dist matrix
+# Count occurences of classes. Indexed from 1. NA:0 is index 8.
+get.cls.counts <- function(CLS, sym=T) {
+  if (sym) {
+    stopifnot(dim(CLS)[1] == dim(CLS)[2])
+    C <- CLS[upper.tri(CLS)]
+  } else {
+    C <- c(CLS)
+  }
+  # indexed from 1. NA is index 8
+  if(any(CLS==0)) stop("Set NA CLS==0 to 8.")
+  cls.counts <- sapply(c(1:8), function(i) sum(C==i))
+  # account for arbitrary symmetry across diagonal for XiY and YiX classes
+  if (sym) {
+    cls.counts[1] = cls.counts[1] + cls.counts[3]
+    cls.counts[3] = 0
+  }
+  cls.counts
+}
+# Calculate coherence for a cls given a count of other classes.
+#   cls.counts enumerated from 1. NA is index 8 (not index 0)
+get.coh <- function(cls.counts, cls) {
+  stopifnot(length(cls.counts) == 8)
+  stopifnot(cls >= 1 && cls <= 8)
+  n <- sum(cls.counts)
+  sigma <- sum(cls.counts * G.DIST[cls,])
+  e <- 1-(sigma/n/4)
+  (e-0.5)*2 # center at 0, range -1 to 1
+}
+# Calculate coherence coherence vector for all classes
+#   cls.counts enumerated from 1. NA is index 8 (not index 0)
+get.coh.vec <- function(CLS, sym=T) {
+  if (length(CLS) == 1) sym<-F
+  cls.counts <- get.cls.counts(CLS, sym)
+  sapply(1:8, function(i) get.coh(cls.counts, i))
+}
+
+# Return most coherent class enumeration given coherence vector.
+#   coh.v enumerated from 1. NA is index 8 (not index 0)
+choose.coh.cls <- function(coh.v, min.coh=0.74) {
+  max.coh <- max(coh.v)
+  max.coh.i <- coh.v == max.coh
+  #print(coh.v)
+  #print(max.coh.i)
+  # coherence below threshold. Return
+  R <- list(); R$coh <- max.coh
+  b <- max.coh < min.coh
+  if (is.na(b)) {
+    print(coh.v)
+    stop("BAD")
+  }
+  if (b) {
+    R$cls <- 4
+  } else {
+    if (sum(max.coh.i)==1) {
+      R$cls <- which(max.coh.i)
+    # Handle ties.
+    # 1. ignore NA class
+    } else if (sum(max.coh.i[1:7])==1) {
+      R$cls <- which(max.coh.i[1:7])
+    # 1.5 ignore NA and UNL class
+    } else if (sum(max.coh.i[c(1,2,3,5,6,7)]) == 1) {
+      max.coh.i[4] <- FALSE
+      R$cls <- which(max.coh.i[1:7])
+    # 2. if ties for opposite signs, return UNL
+    } else if (any(max.coh.i[c(1,2,3)]) & any(max.coh.i[c(5,6,7)])) {
+      R$cls <- 4
+    # 3. if all of a sign or both asyms of a sign, return sym of that sign
+    } else if (all(max.coh.i[c(1,2,3)]) | all(max.coh.i[c(1,3)])) {
+      R$cls <- 2
+    } else if (all(max.coh.i[c(5,6,7)]) | all(max.coh.i[c(5,7)])) {
+      R$cls <- 6
+    # 4. if an asym and sym of the same sign, return asym
+    } else if (all(max.coh.i[c(1,2)]) | all(max.coh.i[c(2,3)])) {
+      R$cls <- 2
+    } else if (all(max.coh.i[c(5,6)]) | all(max.coh.i[c(6,7)])) {
+      R$cls <- 6
+    }
+  }
+  R
+}
+
+# Return collapsed class, coherence, and overall coherence by indices.
+collapse.cls <- function(CLS, idx) {
+  CLS[CLS==0] <- 8
+  idx <- as.factor(idx)
+  n <- length(levels(idx))
+  R <- list()
+  R$idx <- idx
+  R$SIZE <- matrix(0, nrow=n, ncol=n)
+  R$CLS <- matrix(0, nrow=n, ncol=n)
+  R$COH <- matrix(0.0, nrow=n, ncol=n)
+  R$members <- split(rownames(CLS), idx)
+  # compute overall coherence
+  for (i in 1:length(levels(idx))) {
+    for (j in 1:length(levels(idx))) {
+      iv <- idx == levels(idx)[i]
+      jv <- idx == levels(idx)[j]
+      C <- CLS[iv,jv]
+      r <- choose.coh.cls(get.coh.vec(C, sym=i==j))
+      #print(paste(i,j,sum(iv),sum(jv),sep=", "))
+      #print(C)
+      #print(r)
+      if (is.null(r$cls)) {
+        print(C)
+        print(r)
+        stop("BAD.")
+      }
+      R$CLS[i,j] <- r$cls
+      R$COH[i,j] <- r$coh
+      if (i == j) {
+        stopifnot(sum(iv)==sum(jv))
+        R$SIZE[i,j] <- sum(iv)*(sum(iv)-1)/2
+      } else {
+        R$SIZE[i,j] <- sum(iv)*sum(jv)
+      }
+    }
+  }
+  R
+}
+
+get.scores <- function(coh, size) {
+  nn <- sum(size)
+  p <- size/nn
+  ln.p <- log(p,base=2)
+  p.ln.p <- -p*ln.p
+  p.ln.p[is.na(p.ln.p)] <- 0
+  R <- list()
+  R$wavg <- sum(p*coh)
+  R$hwavg <- sum(p.ln.p*coh)
+  R
+}
+
+get.coh.M.score <- function(COLLAPSED) {
+  R <- list()
+  tri <- upper.tri(COLLAPSED$COH,diag=F)
+  diag <- upper.tri(COLLAPSED$COH,diag=T) & !tri
+  R$tri <- get.scores(COLLAPSED$COH[tri], COLLAPSED$SIZE[tri])
+  R$diag <- get.scores(COLLAPSED$COH[diag], COLLAPSED$SIZE[diag])
+  R
+}  
+
+
+pdf("/dev/null")
+GOLD.R <- splom(D.expr.gold$CLS, D.expr.gold$DCOR); dev.off();
+H <- as.hclust(GOLD.R$Rhclust)
+CLS <- D.expr.gold$CLS
+#idx <- cutree(H,k=7)
+#R <- collapse.cls(CLS, idx)
+#get.coh.M.score(R)
+
+
+get.compression <- function(CLS, H) {
+  n <- round(dim(CLS)[1]/2)+2
+  R <- list()
+  R$e.t <- rep(0,n); R$e.c <- rep(0,n)
+  R$w.t <- rep(0,n); R$w.c <- rep(0,n)
+  for(k in 1:n) {
+    print(k)
+    idx <- cutree(H,k)
+    C <- collapse.cls(CLS,idx)
+    S <- get.coh.M.score(C)
+    R$e.t[k] <- S$tri$hwavg
+    R$w.t[k] <- S$tri$wavg
+    R$e.c[k] <- S$diag$hwavg
+    R$w.c[k] <- S$diag$wavg
+  }
+  R
+}
+
+Z <- get.compression(CLS,H)
+
+## H <- as.hclust(TRANS.R$Rhclust)
+## CLS <- D.expr.trans$CLS
+## ZZ <- get.compression(CLS, H)
+
+## idx <- cutree(H,k=2)
+## R <- collapse.cls(CLS,idx)
+## get.coh.M.score(R)
