c4814f77b91764af627b320ac88a66f01c0451c0
Andrew Yates <ayates@node1.cl.osumc.edu>
2013-06-09 18:46:10 -0400

    progress towards all transcription factors

--- jun8_new_gold_gsplom.R
+++ jun8_new_gold_gsplom.R
@@ -1,5 +1,6 @@
 # bucki paths
 library(celegansceentrezg.db)
+library(energy)
 source("~/code/dependency_glyph_splom/lib.R")
 gold.genes <- c("pal-1", "tbx-8", "tbx-9", "elt-1", "cwn-1", "hnd-1", "scrt-1", "elt-3", "nhr-25", "nob-1", "vab-7", "unc-120", "hlh-1", "mab-21", "lin-26")
 load("../jun5.GSE2180.SCAN.select.tab.dcor.tab.RData")
@@ -17,7 +18,7 @@ IDS <- paste0(rownames(DCOR),"_at")
 SYMS <- mget(IDS, celegansceentrezgSYMBOL, ifnotfound=NA)
 qq <- na.omit(match(gold.genes, SYMS))
 
-
+# Verify that dCor and Bool values are as expected by reconstructing GOLD gsplom
 GOLD.M <- exprs(E.GSE2180.SCAN)[qq,]
 rownames(GOLD.M) <- SYMS[qq]
 write.table(GOLD.M, file="~/new.gold.celegans.tab", sep="\t", quote=F)
@@ -38,9 +39,22 @@ rownames(BOOL.gold) <- SYMS[qq]
 colnames(BOOL.gold) <- SYMS[qq]
 
 pdf("~/jun8.newgold.gsplom.pdf")
-R.GOLD <- splom(BOOL.gold, DCOR.gold, useRaster=F, MIN=0.32, MAX=1, grid=F, reorder=T)
+R.GOLD <- splom(BOOL.gold, DCOR.gold, asGlyphs=T, useRaster=F, MIN=0.32, MAX=1, grid=F, reorder=T)
+dev.off()
+pdf("~/jun8.newgold.gsplom.dendro.pdf")
+plot(R.GOLD$Rhclust)
 dev.off()
 #SYMS.ALL <- mget(paste0(rownames(S.GSE2180),"_at"), celegansceentrezgSYMBOL, ifnotfound=NA)
 #match(gold.genes, SYMS.ALL) # lin-26 (3565051) is missing
 
-# TODO: all transcription factors
+
+# ----------------------------------------
+# ALL TRANSCRIPTION FACTORS
+# ----------------------------------------
+trans.entrez <- read.table("~/code/c.elegans_transcription_factors/entrez_transcriptome_list.tab", sep='\t', header=T, stringsAsFactors=F)
+dim(trans.entrez)
+#[1] 1977    3
+
+sum(rownames(DCOR) %in% trans.entrez$EntrezID) # 757
+
+
